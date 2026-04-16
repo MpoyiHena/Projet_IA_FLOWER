206215f5051f7964d954e2dd77bf5faeb924d122
MpoyiHena <etu57430@henallux.be>
2026-04-16 12:05:26 +0200

    Ballayage de toute les image du data set traning

--- IA/main.py
+++ IA/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import picture_treatment as pt
 
+pt.callImage()
 
 #voila c'est fait, on a tout les imports pour faire notre IA, on va pouvoir faire du machine learning et du deep learning, c'est parti !
--- IA/picture_treatment.py
+++ IA/picture_treatment.py
@@ -1,12 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import cv2
+import glob
+import os
 
 def callImage():
-    # Charger l'image
-    image = Image.open('flower.jpg')
-
-    # Afficher l'image
-    plt.imshow(image)
-    plt.axis('off')  # Masquer les axes
-    plt.show()
+    # Chemin vers le dossier contenant les images
+    path = r'C:\Dossier E\Henallux\M1\Systeme inteligent\Projet\flowers\Dataset_flowers\Training Data'
+    
+    # Trouver toutes les images .jpeg dans les sous-dossiers
+    images = glob.glob(os.path.join(path, '**', '*.jpeg'), recursive=True)
+    
+    # Afficher chaque image avec un délai de 2 secondes
+    for img_path in images:
+        img = cv2.imread(img_path)
+        cv2.imshow('Image', img)
+        key = cv2.waitKey(20)  # Attendre 2 secondes
+        if key == ord('q'):
+            break
+    
+    cv2.destroyAllWindows()
